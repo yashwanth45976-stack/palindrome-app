48792c7e8dfddd21b6df803a1ec5d46444de6fdd
yashwanth45976-stack <yashwanth45976@gmail.com>
2026-02-25 15:48:01 +0530

    UC11: Object-oriented palindrome service

--- src/palindromecheckerapp.java
+++ src/palindromecheckerapp.java
@@ -1,25 +1,29 @@
-public class palindromecheckerapp {
-    public static void main(String[] args) {
+class PalindromeChecker {
 
-        String input = "Madam In Eden Im Adam";
-
-        String normalized = input.replaceAll("\\s+", "").toLowerCase();
-
-        boolean isPalindrome = true;
+    public boolean checkPalindrome(String input) {
         int start = 0;
-        int end = normalized.length() - 1;
+        int end = input.length() - 1;
 
         while (start < end) {
-            if (normalized.charAt(start) != normalized.charAt(end)) {
-                isPalindrome = false;
-                break;
+            if (input.charAt(start) != input.charAt(end)) {
+                return false;
             }
             start++;
             end--;
         }
+        return true;
+    }
+}
+
+public class palindromecheckerapp{
+    public static void main(String[] args) {
+
+        String input = "madam";
+
+        PalindromeChecker checker = new PalindromeChecker();
+        boolean result = checker.checkPalindrome(input);
 
         System.out.println("Input text: " + input);
-        System.out.println("Normalized text: " + normalized);
-        System.out.println("Is it a Palindrome? : " + isPalindrome);
+        System.out.println("Is it a Palindrome? : " + result);
     }
 }
