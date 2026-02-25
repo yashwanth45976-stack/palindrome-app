9dad14a5c022c0037947c50870f0fb9ea8ccc0eb
yashwanth45976-stack <yashwanth45976@gmail.com>
2026-02-25 15:27:15 +0530

    UC5: Stack-based palindrome checker

--- src/palindromecheckerapp.java
+++ src/palindromecheckerapp.java
@@ -1,46 +1,24 @@
-/**
- * =========================================================
- *  MAIN CLASS - UseCase4PalindromeCheckerApp
- * =========================================================
- *
- * Use Case 4: Character Array Based Palindrome Check
- *
- * Description:
- * This program converts a string into a character array and
- * checks whether it is a palindrome using the two-pointer approach.
- */
+import java.util.Stack;
 
-public class palindromecheckerapp{
-
-    /**
-     * Application entry point
-     * @param args Command-line arguments
-     */
+public class palindromecheckerapp {
     public static void main(String[] args) {
 
-        // Hardcoded input string
         String input = "madam";
+        Stack<Character> stack = new Stack<>();
 
-        // Convert string to character array
-        char[] chars = input.toCharArray();
-
-        // Two-pointer initialization
-        int start = 0;
-        int end = chars.length - 1;
+        for (int i = 0; i < input.length(); i++) {
+            stack.push(input.charAt(i));
+        }
 
         boolean isPalindrome = true;
 
-        // Two-pointer comparison
-        while (start < end) {
-            if (chars[start] != chars[end]) {
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) != stack.pop()) {
                 isPalindrome = false;
                 break;
             }
-            start++;
-            end--;
         }
 
-        // Display result
         System.out.println("Input text: " + input);
         System.out.println("Is it a Palindrome? : " + isPalindrome);
     }
